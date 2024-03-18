4528df7da6c90ca37f11cf43d3a4a26475eeb917
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-03-18 18:42:18 +0900

    Update 별 찍기 － 8.py

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -2,5 +2,5 @@ num = int(input())
 
 for i in range(1, num+1):
     print("*" * i + " " * ((num * 2) - (i * 2)) + "*" * i)
-for i in range(num-1, 0, -1):
+for i in range(num-1, 0, -1): # 감소하는 for문을 작성할 때는 세 번째 매개변수 작성하는 것을 기억하기
     print("*" * i + " " * ((num * 2) - (i * 2)) + "*" * i)
