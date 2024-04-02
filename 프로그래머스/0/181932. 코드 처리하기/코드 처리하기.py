172284a1394662d21dead699320d9eac3b0db12a
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-04-02 19:34:54 +0900

    [level 0] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,33 @@
+def solution(code):
+    answer = ''
+    mode = 0
+    
+    for i in range(len(code)):
+        if mode == 0:
+            if i % 2 == 0:
+                if code[i] == "1":
+                    mode = 1
+                else:
+                    answer += code[i]
+            else:
+                if code[i] == "1":
+                    mode = 1
+                else:
+                    continue
+                    
+        elif mode == 1:
+            if i % 2 != 0:
+                if code[i] == "1":
+                    mode = 0
+                else:
+                    answer += code[i]
+            else:
+                if code[i] == "1":
+                    mode = 0
+                else:
+                    continue
+                    
+    if len(answer) == 0:
+        answer = "EMPTY"
+
+    return answer
