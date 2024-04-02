b9f94d57384dcb581d83d1d15e53616460b490d2
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-04-02 18:57:22 +0900

    Update 조건 문자열.py

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -1,2 +1,34 @@
+# 간단한 식
 def solution(ineq, eq, n, m):
     return int(eval(str(n) + ineq + eq.replace("!", "") + str(m)))
+
+
+# 내 풀이
+# def solution(ineq, eq, n, m):
+#     answer = 0
+    
+#     if ineq == "<":
+#         if eq == "=":
+#             if n <= m:
+#                 answer += 1
+#             else:
+#                 answer += 0
+#         elif eq == "!":
+#             if n < m:
+#                 answer += 1
+#             else:
+#                 answer += 0
+            
+#     elif ineq == ">":
+#         if eq == "=": 
+#             if n >= m:
+#                 answer += 1
+#             else:
+#                 answer += 0
+#         elif eq == "!":
+#             if n > m:
+#                 answer += 1
+#             else:
+#                 answer += 0
+                
+#     return answer
