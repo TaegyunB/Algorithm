b7d87e7b9d7e10be9b54d23a4ca9d4fefa4c25ab
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-04-02 19:49:29 +0900

    Update 코드 처리하기.py

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -1,33 +1,38 @@
+# 간단한 코드
 def solution(code):
-    answer = ''
-    mode = 0
-    
-    for i in range(len(code)):
-        if mode == 0:
-            if i % 2 == 0:
-                if code[i] == "1":
-                    mode = 1
-                else:
-                    answer += code[i]
-            else:
-                if code[i] == "1":
-                    mode = 1
-                else:
-                    continue
-                    
-        elif mode == 1:
-            if i % 2 != 0:
-                if code[i] == "1":
-                    mode = 0
-                else:
-                    answer += code[i]
-            else:
-                if code[i] == "1":
-                    mode = 0
-                else:
-                    continue
-                    
-    if len(answer) == 0:
-        answer = "EMPTY"
+    return "".join(code.split("1"))[::2] or "EMPTY"
 
-    return answer
+# 내 코드
+# def solution(code):
+#     answer = ''
+#     mode = 0
+    
+#     for i in range(len(code)):
+#         if mode == 0:
+#             if i % 2 == 0:
+#                 if code[i] == "1":
+#                     mode = 1
+#                 else:
+#                     answer += code[i]
+#             else:
+#                 if code[i] == "1":
+#                     mode = 1
+#                 else:
+#                     continue
+                    
+#         elif mode == 1:
+#             if i % 2 != 0:
+#                 if code[i] == "1":
+#                     mode = 0
+#                 else:
+#                     answer += code[i]
+#             else:
+#                 if code[i] == "1":
+#                     mode = 0
+#                 else:
+#                     continue
+                    
+#     if len(answer) == 0:
+#         answer = "EMPTY"
+
+#     return answer
