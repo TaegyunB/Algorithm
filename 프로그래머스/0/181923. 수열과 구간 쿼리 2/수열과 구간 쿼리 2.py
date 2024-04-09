704db46d4e70d0a988ca97cd5fde2ae04b3d74e8
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-04-09 21:40:33 +0900

    Update 수열과 구간 쿼리 2.py

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -1,3 +1,20 @@
+# 간단한 코드
+def solution(arr, queries):
+    answer = []
+
+    for s, e, k in queries:
+        tmp = []
+
+        for x in arr[s:e+1]:
+            if x > k:
+                tmp.append(x)
+        answer.append(-1 if not tmp else min(tmp)) 
+        # 리스트 tmp가 비어있지 않은 경우에는 tmp 리스트 안의 최솟값을 answer 리스트에 추가하고, 비어있는 경우에는 -1을 추가합니다.
+        # 'not tmp': 리스트 'tmp'가 비어있지 않으면 'True', 비어있으면 'False'
+    
+    return answer
+
+# 내 코드
 def solution(arr, queries):
     answer = []
     
