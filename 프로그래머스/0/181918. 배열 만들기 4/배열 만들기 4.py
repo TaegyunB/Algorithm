afb5647d61e6fcb1dd05db4895ca4aa4f19423f2
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-04-10 15:13:29 +0900

    Update 배열 만들기 4.py

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -1,3 +1,14 @@
+# 간단한 풀이
+def solution(arr):
+    stk = []
+
+    for i in range(len(arr)):
+        while stk and stk[-1] >= arr[i]: # 스택이 비어있지 않고, 스택의 맨 위에 있는 값이 현재 숫자 arr[i]보다 크거나 같은 경우에는 반복
+            stk.pop()
+        stk.append(arr[i])
+    return stk
+
+# 내 풀이
 def solution(arr):
     stk = []
     i = 0
