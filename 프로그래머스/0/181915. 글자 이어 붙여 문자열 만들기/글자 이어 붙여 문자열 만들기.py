04e51602701615ed2f58ca6e5636ad0121a8a7c7
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-04-11 12:39:42 +0900

    Update 글자 이어 붙여 문자열 만들기.py

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
@@ -1,8 +1,12 @@
+# 간단한 풀이
+def solution(my_string, index_list):
+    return ''.join([my_string[idx] for idx in index_list]) # 리스트 컴프리헨션을 사용하여 'index_list'에 있는 각 인덱스에 대해 해당하는 문자를 선택함
+
+# 내 풀이
 def solution(my_string, index_list):
     answer = ''
     
     for i in index_list:
         answer += my_string[i]
-        
     
     return answer
