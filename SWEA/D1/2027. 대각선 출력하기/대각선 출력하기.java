c653058aa6df78b604085c10dad5fec32d0068ac
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2025-01-19 23:57:17 +0900

    [D1] Title: 대각선 출력하기, Time: 133 ms, Memory: 20,352 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        
+        System.out.print("#++++\n+#+++\n++#++\n+++#+\n++++#");
+	}
+}
