c838763ae2a39b4d3e7222d366195dee283c4ffd
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2025-03-18 20:21:31 +0900

    [D2] Title: +=, Time: 147 ms, Memory: 32,868 KB -BaekjoonHub

--- SWEA/D2/21425. ＋＝/＋＝.java
+++ SWEA/D2/21425. ＋＝/＋＝.java
@@ -6,22 +6,22 @@ class Solution
 	public static void main(String args[]) throws Exception
 	{
 		Scanner sc = new Scanner(System.in);
-		
+        
         int T = sc.nextInt();
         
-        for(int i=0; i<T; i++){
-         	int A = sc.nextInt();
-            int B = sc.nextInt();
-            int N = sc.nextInt();
+        for(int i=0; i<T; i++) {
+        	int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
             int count = 0;
             
-            while(A <= N && B <= N){ // A와 B 모두가 N 이하이어야 하기 때문에 && 사용
-               	if(A > B) {
-             		B += A;   
-            	}
-            	else {
-             		A += B;   
-            	}
+            while(a <= n && b <= n) {
+                if(a > b) {
+                 	b += a;   
+                }
+                else {
+                 	a += b;   
+                }
                 count++;
             }
             System.out.println(count);
