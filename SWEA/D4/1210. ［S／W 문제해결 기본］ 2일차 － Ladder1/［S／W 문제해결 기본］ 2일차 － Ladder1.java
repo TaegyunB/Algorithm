8dc448c4a90b776c7a04a06803ca546bbddc765f
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2025-06-06 19:34:53 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 136 ms, Memory: 39,584 KB -BaekjoonHub

---
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = 10;
+		for (int tc = 1; tc <= T; tc++) {
+
+			int N = Integer.parseInt(br.readLine());
+			int[][] arr = new int[100][100];
+			for (int i = 0; i < 100; i++) {
+				String[] input = br.readLine().split(" ");
+
+				for (int j = 0; j < 100; j++) {
+					arr[i][j] = Integer.parseInt(input[j]);
+				}
+			}
+
+			for (int y = 0; y < 100; y++) {
+				int x = 0;
+				if (arr[x][y] == 1) {
+					int ni = x + 1;
+					int nj = y + 0;
+
+					while (ni < 100) {
+
+						// 왼쪽
+						if (nj - 1 >= 0 && arr[ni][nj - 1] == 1) {
+							while (nj - 1 >= 0 && arr[ni][nj - 1] == 1) {
+								nj--;
+							}
+						}
+						// 오른쪽
+						else if (nj + 1 < 100 && arr[ni][nj + 1] == 1) {
+							while (nj + 1 < 100 && arr[ni][nj + 1] == 1) {
+								nj++;
+							}
+						}
+
+						ni++;
+					}
+
+					if (arr[ni - 1][nj] == 2) {
+						System.out.printf("#%d %d\n", tc, y);
+						break;
+					}
+				}
+			}
+		}
+
+	}
+}
