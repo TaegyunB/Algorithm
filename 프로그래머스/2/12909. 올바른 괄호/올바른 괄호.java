e52829f57a57b82aa3e8adf695f2b03beaef8a6a
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2025-06-22 20:07:43 +0900

    [level 2] Title: 올바른 괄호, Time: 6.82 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,37 +1,26 @@
-import java.util.*;
-
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
 
-        char[] letters = new char[s.length()];
-        for(int i=0; i<s.length(); i++) {
-            letters[i] = s.charAt(i);
-        }
+        int count = 0;
         
-        Stack<Character> stack = new Stack<>();  // 스택 선언
-        
-        for(int i=0; i<letters.length; i++) {
-            if(letters[0] == ')') {
+        for(char c : s.toCharArray()) {
+            if(c == '(') {
+                count++;
+            } else {
+                count--;
+            }
+            
+            if(count < 0) {
                 answer = false;
                 break;
             }
-            
-            if(letters[i] == '(') {
-                stack.push(letters[i]);
-            } else {
-                if(stack.isEmpty()) {
-                    answer = false;
-                    break;
-                }
-                stack.pop();
-            }
         }
         
-        if(!stack.isEmpty()) {
+        if(count != 0) {
             answer = false;
         }
-        
+
         return answer;
     }
 }
