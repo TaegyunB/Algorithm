5cfee6b92f198fb9cc27513a01482825db63342c
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-01-14 19:12:47 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 127 ms, Memory: 36,196 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 public class Solution {
 
@@ -8,66 +9,71 @@ public class Solution {
 		
 		for (int tc=1; tc<=10; tc++) {
 			int tcNum = Integer.parseInt(br.readLine());
-			int[][] arr = new int[100][100];
+			
+			// 각 행의 합을 저장하는 배열
+			int[] rowSum = new int[100];
+			
+			// 각 열의 합을 저장하는 배열
+			int[] colSum = new int[100];
+			
+			// 왼쪽 위 -> 오른쪽 아래 대각선 합
+			int diag1 = 0;
+			
+			// 오른쪽 위 -> 왼쪽 아래 대각선 합
+			int diag2 = 0;
+			
+			// 모든 합 중 최대값
+			int maxSum = 0;
 			
 			// 100 x 100 배열에 값 추가
 			for (int i=0; i<100; i++) {
-				String[] numList = br.readLine().split(" ");
+//				String[] numList = br.readLine().split(" ");
+				StringTokenizer st = new StringTokenizer(br.readLine());  // split 보다 빠름
 				
-				for (int j=0; j<numList.length; j++) {
-					arr[i][j] = Integer.parseInt(numList[j]);
+				for (int j=0; j<100; j++) {
+					int v = Integer.parseInt(st.nextToken());
+					
+					rowSum[i] += v;  // i번째 행의 합 누적
+					colSum[j] += v;  // j번째 행의 합 누적
+					
+					// 왼쪽 위 -> 오른쪽 아래 대각선
+					if (i == j) {
+						diag1 += v;
+					} 
+					
+					// 오른쪽 위 -> 왼쪽 아래 대각선
+					if (i + j == 99) {
+						diag2 += v;
+					}
 				}
-			}
-			
-			int maxSum = 0;  // 최대값
-			
-			// 행의 합
-			for (int x=0; x<100; x++) {
-				int sum = 0;
-				
-				for (int y=0; y<100; y++) {
-					sum += arr[x][y]; 
-				}
-				
-				if (sum > maxSum) {
-					maxSum = sum;
-				}
-			}
-			
-			// 열의 합
-			for (int y=0; y<100; y++) {
-				int sum = 0;
-				
+
+				// 현재까지 누적된 행/열/대각선 합 중 최대값 갱신
 				for (int x=0; x<100; x++) {
-					sum += arr[x][y];
+					
+					// 행의 합 중 최대
+					if (rowSum[x] > maxSum) {
+						maxSum = rowSum[x];
+					}
+					
+					// 열의 합 중 최대
+					if (colSum[x] > maxSum) {
+						maxSum = colSum[x];
+					}
+					
+					// 첫 번째 대각선과 비교
+					if (diag1 > maxSum) {
+						maxSum = diag1;
+					}
+					
+					// 두 번째 대각선과 비교
+					if (diag2 > maxSum) {
+						maxSum = diag2;
+					}
 				}
-				
-				if (sum > maxSum) {
-					maxSum = sum;
-				}
-			}
-			
-			// 오른쪽 아래 대각선의 합
-			int rightCrossSum = 0;
-			for (int x=0; x<100; x++) {
-				rightCrossSum += arr[x][x];
-			}
-			
-			if (rightCrossSum > maxSum) {
-				maxSum = rightCrossSum;
-			}
-			
-			// 왼쪽 아래 대각선의 합
-			int leftCrossSum = 0;
-			for (int x=99; x>=0; x--) {
-				leftCrossSum += arr[x][99-x];
-			}
-			
-			if (leftCrossSum > maxSum) {
-				maxSum = leftCrossSum;
 			}
 			
 			sb.append("#").append(tc).append(" ").append(maxSum).append("\n");
+			
 		}
 		
 		System.out.println(sb);
