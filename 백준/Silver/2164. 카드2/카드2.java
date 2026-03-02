9c7b6cae1dcc3da6b9a98afc119edd2c9f34de3c
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-03-02 16:51:01 +0900

    [Silver IV] Title: 카드2, Time: 172 ms, Memory: 36824 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		Deque<Integer> q = new ArrayDeque<>();
+		
+		for (int i=1; i<=N; i++) {
+			q.offer(i);
+		}
+		
+		while (q.size() != 1) {
+			q.pollFirst();
+			int x = q.pollFirst();
+			q.offerLast(x);
+		}
+		
+		System.out.println(q.peek());
+	}
+}
