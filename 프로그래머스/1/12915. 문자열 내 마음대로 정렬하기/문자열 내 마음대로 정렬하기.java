007a23817ae68738905d11a6ef8f5974491fc944
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-03-12 21:24:40 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 13.06 ms, Memory: 91.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        List<String> list = new ArrayList<>();
+        
+        for (String str : strings) {
+            list.add(str.charAt(n) + str);
+        }
+        
+        Collections.sort(list);
+        
+        String[] answer = new String[list.size()];
+        
+        for (int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i).substring(1);
+        }
+        
+        return answer;
+    }
+}
