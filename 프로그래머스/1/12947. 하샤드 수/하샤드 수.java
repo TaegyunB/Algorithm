4fbdd981ff428ded2dc1c634824a0499c41a3357
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-04-18 00:42:09 +0900

    [level 1] Title: 하샤드 수, Time: 0.20 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean solution(int x) {
+        String s = String.valueOf(x);
+        int sum = 0;
+        
+        for (int i=0; i<s.length(); i++) {
+            sum += s.charAt(i) - '0';
+        }
+        
+        System.out.println(sum);
+        
+        if (x % sum == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
