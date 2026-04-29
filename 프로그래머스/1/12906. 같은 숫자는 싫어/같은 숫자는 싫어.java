9ea6632320700f66c6d9d19d6c3346ff0113ca04
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-04-29 15:29:35 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 51.14 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,17 +2,16 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        // 스택 선언
-        Stack<Integer> stack = new Stack<>();
         
-        for (int i=0; i<arr.length; i++) {
-            if (stack.isEmpty()) {
-                stack.add(arr[i]);
-            } else if(stack.peek() != arr[i]) {
-                stack.add(arr[i]);
+        Deque<Integer> stack = new ArrayDeque<>();  // 스택 선언
+        stack.offerLast(arr[0]);  // arr의 첫번째 값을 stack에 추가
+        
+        for (int i=1; i<arr.length; i++) {  // arr의 2번째부터 마지막까지 순회하면서
+            if (stack.peekLast() != arr[i]) {  // stack의 마지막 값이 다음 추가할 값이랑 같으면 안되기 때문에 다르다면
+                stack.offerLast(arr[i]);  // arr[i] 값을 stack에 추가
             }
         }
         
-        return stack.stream().mapToInt(i -> i).toArray();
+        return stack.stream().mapToInt(x -> x).toArray();  // stack을 배열로 변환해서 출력
     }
 }
