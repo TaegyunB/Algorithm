179bcffb75398ab8750518f850d17a1ece65270d
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-05-11 16:13:32 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.03 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -6,21 +6,24 @@ class Solution {
         int bigIdx = 0;
         
         while (flag) {
+            // 접기 전에 먼저 지갑에 들어가는지 체크 (그대로 or 90도 회전)
             if (wallet[0] >= bill[0] && wallet[1] >= bill[1]) {
                 break;
             } else if (wallet[0] >= bill[1] && wallet[1] >= bill[0]) {
                 break;
             }
             
+            // 긴 쪽 인덱스 설정
             if (bill[0] > bill[1]) {
                 bigIdx = 0;
             } else if (bill[0] < bill[1]) {
                 bigIdx = 1;
             }
             
-            bill[bigIdx] /= 2;
+            bill[bigIdx] /= 2;  // 긴 쪽 반으로 접기
             cnt++;
             
+            // 접은 후 다시 체크 (그대로 or 90도 회전)
             if (wallet[0] >= bill[0] && wallet[1] >= bill[1]) {
                 break;
             } else if (wallet[0] >= bill[1] && wallet[1] >= bill[0]) {
