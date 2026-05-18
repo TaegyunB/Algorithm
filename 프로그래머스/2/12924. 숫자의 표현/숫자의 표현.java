073609f3b9424b812774034adf010c42e8e17bf2
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-05-18 13:31:08 +0900

    [level 2] Title: 숫자의 표현, Time: 0.43 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,20 +1,22 @@
 class Solution {
     public int solution(int n) {
-        int cnt = 1;
+        // 투 포인터/슬라이딩 윈도우 풀이
+        int left = 1;
+        int right = 1;
+        int sum = 1;
+        int cnt = 0;
         
-        for (int i=1; i<n; i++) {
-            int sum = i;
-            for (int j=i+1; j<=n; j++) {
-                if (sum == n) {
-                    cnt++;
-                    break;
-                }
-                
-                if (sum > n) {
-                    break;
-                }
-                
-                sum+=j;
+        while (left <= n) {
+            if (sum == n) {  // 연속한 자연수 합한 값이 n과 같다면
+                cnt++;  // 카운트 증가하고
+                sum -= left;  // 왼쪽 포인터가 가리키는 값을 빼고
+                left++;  // 왼쪽 포인터를 다음 값으로 이동
+            } else if (sum < n) {  // 연속한 자연수 합한 값이 n보다 작으면 -> 오른쪽 포인터를 계속 증가시킬 수 있음
+                right++;  // 오른쪽 포인터를 증가
+                sum += right;  // sum에 값 추가
+            } else if (sum > n) {  // 연속한 자연수 합한 값이 n보다 크면 -> 왼쪽 포인터가 가르키고 있는 값을 빼고 증가시켜야함
+                sum -= left;  // sum에 현재 왼쪽 포인터가 가르키고 있는 값을 빼고
+                left++;  // 왼쪽 포인터를 증가
             }
         }
         
