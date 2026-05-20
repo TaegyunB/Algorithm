24fb279bcffd724cd5fc589effe55df0fc4ca438
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-05-20 12:17:58 +0900

    [level 1] Title: 최소직사각형, Time: 0.70 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,9 +1,13 @@
 class Solution {
     public int solution(int[][] sizes) {
+        // 명함을 가로 세로 눕혀서 수납할 수 있기 때문에 가로 세로 비교하면서 가로 세로 중 큰 값을 가로로, 작은 값을 세로로 통일
+        // maxWidth, maxHeight을 비교하면서 가로에서 제일 큰 값, 세로에서 제일 큰 값을 계속 각각 갱신
+        // 최종으로 나온 가로와 세로를 곱한 값이 정답
+        
         int answer = 0;
         
-        int maxHeight = 0;
         int maxWidth = 0;
+        int maxHeight = 0;
         
         for (int i=0; i<sizes.length; i++) {
             int width = sizes[i][0];
@@ -12,19 +16,19 @@ class Solution {
             
             if (height > width) {
                 temp = width;
-                sizes[i][0] = height;
-                sizes[i][1] = temp;
+                width = height;
+                height = temp;
             }
             
-            if (sizes[i][0] > maxHeight) {
-                maxHeight = sizes[i][0];
+            if (width > maxWidth) {
+                maxWidth = width;
             }
             
-            if (sizes[i][1] > maxWidth) {
-                maxWidth = sizes[i][1];
+            if (height > maxHeight) {
+                maxHeight = height;
             }
         }
         
-        return maxHeight * maxWidth;
+        return maxWidth * maxHeight;
     }
 }
