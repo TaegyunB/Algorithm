b847acc0c8edb9c6b85010ddb859f564f9d83356
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-05-28 17:14:16 +0900

    [level 1] Title: 명예의 전당 (1), Time: 3.14 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int k, int[] score) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        List<Integer> answer = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();  // 우선순위 큐
+        List<Integer> answer = new ArrayList<>();  // 정답 리스트
         
         for (int i=0; i<score.length; i++) {
-            pq.offer(score[i]);
+            pq.offer(score[i]);  // pq에 추가
             
-            if (pq.size() > k) {
+            if (pq.size() > k) {  // pq 사이즈가 k보다 크다면 제일 낮은 점수 하나 빼야함
                 pq.poll();
             }
             
-            answer.add(pq.peek());
+            answer.add(pq.peek());  // 제일 낮은 점수를 answer에 추가
         }
         
         return answer.stream().mapToInt(x -> x).toArray();
