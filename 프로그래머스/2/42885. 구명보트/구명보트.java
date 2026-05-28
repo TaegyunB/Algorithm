336eea61d8b5ea860f0db42fee41b46910915888
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-05-28 19:40:17 +0900

    [level 2] Title: 구명보트, Time: 5.78 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,25 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        // 실수 - 최대 2명씩 밖에 탈 수 없다는 것을 까먹고 있었음
-        Arrays.sort(people);
+        Arrays.sort(people);  // 먼저 정렬을 한 후
         
+        // 투 포인터로 풀 것이기 때문에 left, right 지정
         int left = 0;
         int right = people.length - 1;
         
-        int cnt = 0;
+        int cnt = 0;  // 보트의 수
         
-        while (left <= right) {
-            int sum = people[left] + people[right];
+        while (left <= right) {  // left가 right보다 크지 않을 때 까지
+            int sum = people[left] + people[right];  // 두 사람의 몸무게를 더한 값
             
-            if (sum <= limit) {
-                left++;
-                right--;
-                cnt++;
-            } else {
-                right--;
-                cnt++;
+            if (sum <= limit) {  // 합이 limit보다 작으면 두 사람을 모두 태울 수 있으니깐
+                left++;  // 다음 사람으로
+                right--;  // 다음 사람으로
+            } else {  // 합이 limit보다 크다는 것은 한 사람만 태울 수 있다는 뜻이니깐
+                right--;  // 큰 값을 먼저 다음으로
             }
+            
+            cnt++;  // 보트 수 추가
         }
         
         return cnt;
