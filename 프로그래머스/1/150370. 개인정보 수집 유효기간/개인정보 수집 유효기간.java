a7e9cb4197ab006fb57dddce463207d19c27536e
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-05-29 12:33:20 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 12.01 ms, Memory: 63 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        List<Integer> answer = new ArrayList<>();
+        
+        // today
+        String[] todaySplit = today.split("\\.");
+        
+        int tyear = Integer.parseInt(todaySplit[0]);
+        int tmonth = Integer.parseInt(todaySplit[1]);
+        int tday = Integer.parseInt(todaySplit[2]);
+        
+        System.out.println(tyear + " " + tmonth + " " + tday);
+        
+        // terms
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for (int i=0; i<terms.length; i++) {
+            String[] term = terms[i].split(" ");
+            
+            map.put(term[0].charAt(0), Integer.parseInt(term[1]));
+        }
+        
+        // privacies
+        for (int i=0; i<privacies.length; i++) {
+            String privacy = privacies[i];
+            String[] priSplit = privacy.split(" ");
+            // System.out.println(priSplit[0] + " " + priSplit[1]);
+            
+            String availableDate = priSplit[1];
+            
+            // System.out.println(availableDate);
+            
+            String[] dateSplit = priSplit[0].split("\\.");
+            int year = Integer.parseInt(dateSplit[0]);
+            int month = Integer.parseInt(dateSplit[1]);
+            int date = Integer.parseInt(dateSplit[2]);
+            
+            // System.out.println(year + " " + month + " " + date);
+            
+            int getMonth = map.get(availableDate.charAt(0));
+            // System.out.println(getMonth);
+            
+            month += getMonth;
+            while (month > 12) { // 유효기간은 1 이상 100 이하이기 때문에
+                year++;
+                month -= 12;
+            }
+            
+            System.out.println(year + " " + month + " " + date);
+            
+            // i + 1
+            if (year < tyear) {
+                answer.add(i+1);
+            } else if (year == tyear && month < tmonth) {
+                answer.add(i+1);
+            } else if (year == tyear && month == tmonth && date <= tday) {
+                answer.add(i+1);
+            }
+        }
+            
+            
+        return answer.stream().mapToInt(x -> x).toArray();
+    }
+}
