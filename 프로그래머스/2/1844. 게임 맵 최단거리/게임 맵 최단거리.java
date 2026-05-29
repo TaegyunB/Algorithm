0818ad0f144c4c3402638818a30f20b10187bd6d
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-05-29 13:35:40 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.60 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,61 +2,60 @@ import java.util.*;
 
 class Solution {
     
-    // 위치와 거리 정보를 담는 클래스
-    static class Pos {
-        int x, y, dist;  // x좌표, y좌표, 시작점부터의 거리
-        
-        Pos(int x, int y, int dist) {
-            this.x = x;
-            this.y = y;
-            this.dist = dist;
-        }
-    }
+    static int[] dx = new int[]{0, 1, 0, -1};
+    static int[] dy = new int[]{1, 0, -1, 0};
+    static boolean[][] visited;
+    static int n, m;
     
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
     
     public int solution(int[][] maps) {
         
-        int n = maps.length;
-        int m = maps[0].length;
+        n = maps.length;
+        m = maps[0].length;
         
-        boolean[][] visited = new boolean[n][m];
-        int result = bfs(maps, 0, 0, visited, n, m);
+        visited = new boolean[n][m];
+        return bfs(maps, 0, 0);
         
-        return result;
     }
     
-    // BFS를 이용한 최단거리 탐색 함수
-    private int bfs(int[][] maps, int x, int y, boolean[][] visited, int n, int m) {
-        // BFS를 위한 큐 생성
-        Queue<Pos> q = new ArrayDeque<>();
+    static int bfs(int[][] maps, int startx, int starty)  {
+        Deque<Pos> q = new ArrayDeque<>();
+        q.offer(new Pos(startx, starty, 1));
+        visited[startx][starty] = true;
         
-        q.offer(new Pos(x, y, 1));
-        visited[x][y] = true;
-        
-        // 쿠가 빌 때까지 반복
         while (!q.isEmpty()) {
-            Pos c = q.poll();
-            int cx = c.x;
-            int cy = c.y;
-            int dist = c.dist;
+            Pos pos = q.poll();
+            int x = pos.x;
+            int y = pos.y;
+            int dist = pos.dist;
             
             for (int i=0; i<4; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 
                 if (nx == n-1 && ny == m-1) {
                     return dist+1;
                 }
                 
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1 && visited[nx][ny] == false) {
-                    q.offer(new Pos(nx, ny, dist + 1));
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1 && !visited[nx][ny]) {
                     visited[nx][ny] = true;
+                    q.offer(new Pos(nx, ny, dist+1));
                 }
             }
         }
         
         return -1;
+    }
+    
+    static class Pos {
+        int x;
+        int y;
+        int dist;
+        
+        public Pos(int x, int y, int dist) {
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+        }
     }
 }
