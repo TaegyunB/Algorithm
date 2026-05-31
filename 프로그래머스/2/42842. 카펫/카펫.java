17c56e2a391e1b03a900b5cb56312ac1a02f8ea4
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-05-31 13:37:49 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 59.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,21 +1,16 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
+        int total = brown + yellow;
         
-        int area = brown + yellow;
-        
-        for (int h = 3; h * h <= area; h++) {  // h * h: 절반 탐색 및 중복 탐색 방지
-            // h * w == area를 구하기 위해 (h, w)쌍을 구하기
-            if (area % h == 0) {
-                int w = area / h;
-                
-                // W >= H >= 3
-                if ((2 * h) + (2 * w) - 4 == brown) {
-                    return new int[]{w, h};
-                }
+        for (int h=3; h<=total; h++) {  // 가로, 세로 무조건 3이상부터 시작
+            
+            int w = total / h;
+            
+            if ((w-2) * (h-2) == yellow) {  // 위 아래 두줄, 좌우 두줄을 제거하고 곱한 값이 yellow
+                return new int[]{w, h};
             }
         }
         
-        return new int[]{};
+        return new int[]{0, 0};
     }
 }
