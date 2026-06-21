e1c765204d757604d42fb29e0aa49bcfe94253ce
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-06-21 16:46:20 +0900

    [level 2] Title: 배달, Time: 15.05 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,73 +1,59 @@
 import java.util.*;
 
 class Solution {
-    
-    static int[] dists;
-    static List<ArrayList<Pos>> tree;
-    
     public int solution(int N, int[][] road, int K) {
+        int[][] graph = new int[N+1][N+1];
         
-        dists = new int[N];
-        tree = new ArrayList<>();
-        
-        for (int i=0; i<N; i++) {
-            tree.add(new ArrayList<>());
-            dists[i] = Integer.MAX_VALUE;
+        for (int i=0; i<=N; i++) {
+            for (int j=0; j<=N; j++) {
+                if (i == j) {
+                    graph[i][j] = 0;
+                } else {
+                    graph[i][j] = Integer.MAX_VALUE;
+                }
+            }
         }
         
         for (int i=0; i<road.length; i++) {
-            int start = road[i][0] - 1;
-            int end = road[i][1] - 1;
+            int start = road[i][0];
+            int end = road[i][1];
             int dist = road[i][2];
             
-            tree.get(start).add(new Pos(end, dist));
-            tree.get(end).add(new Pos(start, dist));
+            graph[start][end] = Math.min(graph[start][end], dist);
+            graph[end][start] = Math.min(graph[end][start], dist);
         }
         
-        dists[0] = 0;
-        dijkstra(0, K);
+        for (int k=1; k<=N; k++) {
+            for (int i=1; i<=N; i++) {
+                for (int j=1; j<=N; j++) {
+                    if (i == j) {
+                        continue;
+                    }
+                    
+                    // Integer.MAX_VALUE + 1 만 해도 오버플로우가 발생하기 때문에 이를 방지
+                    if (graph[i][k] == Integer.MAX_VALUE || graph[k][j] == Integer.MAX_VALUE) {
+                        continue;
+                    }
+                    
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
+            }
+        }
+        
+        for (int i=1; i<=N; i++) {
+            for (int j=1; j<=N; j++) {
+                System.out.print(graph[i][j] + " ");
+            }
+            System.out.println();
+        }
         
         int answer = 0;
-        
-        for (int i=0; i<N; i++) {
-            if (dists[i] <= K) {
+        for (int i=1; i<=N; i++) {  // 1번 마을에서 출발하는 것만 구하면 됨
+            if (graph[1][i] <= K) {
                 answer++;
             }
         }
         
         return answer;
-    }
-    
-    // 다익스트라는 거리가 짧은 노트부터 꺼내야 하기 때문에 PQ를 사용
-    private void dijkstra(int startNode, int K) {
-        Queue<Pos> q = new PriorityQueue<>((a, b) -> a.dist - b.dist);
-        
-        q.offer(new Pos(startNode, 0));
-        
-        while(!q.isEmpty()) {
-            Pos pos = q.poll();
-            int start = pos.end;
-            int dist = pos.dist;
-            
-            for (Pos nextPos : tree.get(start)) {
-                int nextNode = nextPos.end;
-                int nextDist = nextPos.dist;
-                
-                if (dist + nextDist <= K && dist + nextDist <= dists[nextNode]) {
-                    dists[nextNode] = dist + nextDist;
-                    q.offer(new Pos(nextNode, dist + nextDist));
-                }
-            }
-        }   
-    }
-    
-    static class Pos {
-        int end;
-        int dist;
-        
-        public Pos(int end, int dist) {
-            this.end = end;
-            this.dist = dist;
-        }
     }
 }
