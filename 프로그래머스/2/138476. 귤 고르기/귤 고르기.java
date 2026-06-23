08c9f8718088deb1d19a7ed269bf614227667064
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-06-23 20:24:00 +0900

    [level 2] Title: 귤 고르기, Time: 41.75 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -8,6 +8,38 @@ class Solution {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
         
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort((a, b) -> b.getValue() - a.getValue());  // 내림차순
+        
+        // List<Integer> list = new ArrayList<>(map.values());
+        // list.sort(Comparator.reverseOrder());
+        
+        int cnt = 0;
+        int sum = 0;
+        for (Map.Entry<Integer, Integer> entry : list) {
+            sum += entry.getValue();
+            cnt++;
+            
+            if (sum >= k) {
+                return cnt;
+            }
+        }
+        
+        return cnt;
+    }
+}
+
+/***
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int num : tangerine) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
         int[] total = new int[map.size()];
         
         int idx = 0;
@@ -36,3 +68,4 @@ class Solution {
         return 0;
     }
 }
+***/
