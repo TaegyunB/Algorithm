9c137bc699cad81f11e6ed855b1c61d558923e4e
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-06-26 15:35:13 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,45 +1,13 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int a, int b) {
         
-        if (a > b) {
-            int temp = a;
-            a = b;
-            b = temp;
-        }
-        
-        Deque<Integer> q = new ArrayDeque<>();
-        for (int i=1; i<=n; i++) {
-            q.offerLast(i);
-        }
-        
         int round = 0;
-        boolean flag = false;
         
-        while (!flag) {
-            for (int i=1; i<=n/2; i++) {
-                int p1 = q.pollFirst();
-                int p2 = q.pollFirst();
-                
-                if (p1 == a && p2 == b) {
-                    round++;
-                    return round;
-                }
-                
-                if (p1 != a && p2 != a && p1 != b && p2 != b) {
-                    q.offerLast(p1);
-                }
-                
-                if (p1 == a || p2 == a) {
-                    q.offerLast(a);
-                } else if (p1 == b || p2 == b) {
-                    q.offerLast(b);
-                } 
-            }
+        while (a != b) {
+            a = a/2 + a%2;
+            b = b/2 + b%2;
             
             round++;
-            n /= 2;
         }
         
         return round;
