f94f754007b9b856337c36913aa3fbebfe4ae96d
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-08-09 17:11:05 +0900

    [level 2] Title: 더 맵게, Time: 664.10 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -4,20 +4,32 @@ class Solution {
     public int solution(int[] scoville, int K) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        for (int n : scoville) {
-            pq.offer(n);
+        for (int s : scoville) {
+            pq.offer(s);
         }
         
+        
         int cnt = 0;
-        while (pq.peek() < K) {
-            if (pq.size() <= 1) {
+        while (!pq.isEmpty()) {
+            int a = pq.poll();
+            
+            // pq의 첫번째 값이 K보다 크다는 뜻은
+            // 모든 음식의 스코빌 지수를 K 이상으로 만들었다는 뜻
+            if (a >= K) {
+               return cnt;
+            }
+            
+            // 계속 돌면서 pq 사이즈가 1 미만이라는 것은
+            // 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우라는 뜻
+            if (pq.size() < 1) {
                 return -1;
             }
             
-            int first = pq.poll();
-            int second = pq.poll();
+            int b = pq.poll();
             
-            pq.offer(first + (second * 2));
+            int newScoville = a + (b * 2);
+            
+            pq.offer(newScoville);
             cnt++;
         }
         
