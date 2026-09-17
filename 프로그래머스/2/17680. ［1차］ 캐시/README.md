d9b503e011a63bd440c834fdd65c60a2c58edbb9
Taegyun Baek <147308244+TaegyunB@users.noreply.github.com>
2026-09-17 16:50:48 +0900

    [level 2] Title: [1차] 캐시, Time: 33.84 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680#) 
 
 ### 성능 요약
 
-메모리: 126 MB, 시간: 31.46 ms
+메모리: 135 MB, 시간: 33.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 17일 16:32:13
+2026년 09월 17일 16:50:46
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,32 +3,35 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         
+        // cacheSize가 0이면 캐시 교체 알고리즘을 사용할 수 없기 때문에 그냥 다 MISS 처리
         if (cacheSize == 0) {
             return cities.length * 5;
         }
         
-        LinkedList<String> q = new LinkedList<>();
+        LinkedList<String> q = new LinkedList<>();  // Queue로 할라고 했는데 중간에서 제거해야하는 상황도 있어서 LinkedList 사용
+        int processTime = 0;  // 총 실행시간
         
-        int processTime = 0;
-        
+        // 대소문자 구별을 하지 않는다고 해서 모든 도시를 다 대문자로 변환
         String[] upperCaseCities = new String[cities.length];
         for (int i=0; i<cities.length; i++) {
             upperCaseCities[i] = cities[i].toUpperCase();
         }
         
         for (int i=0; i<upperCaseCities.length; i++) {     
+            // cacheSize 보다 작을 때도 겹치는 도시가 있을 수 있으니 
             if (q.size() < cacheSize) {
+                // 겹치면 갱신하는 로직 추가 - HIT
                 if (q.contains(upperCaseCities[i])) {
                     q.remove(q.indexOf(upperCaseCities[i]));
                     q.offerLast(upperCaseCities[i]);
                     processTime += 1;
-                } else {
+                } else { // 겹치지 않으면 그냥 추가 - MISS
                     q.offerLast(upperCaseCities[i]);
                     processTime += 5;
                 }
             } 
             
-            // Queue가 캐시크기만큼 값이 들어있으면
+            // Queue가 cacheSize만큼 값이 들어있으면
             else { 
                 if (!q.contains(upperCaseCities[i])) {  // MISS
                     q.pollFirst();
